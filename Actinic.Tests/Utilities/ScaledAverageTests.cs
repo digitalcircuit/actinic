d897288096897ca76a13aaedcf51d5570ac9af9a
Shane Synan <digitalcircuit36939@gmail.com>
2019-04-10 18:31:11 -0400

    utility: Add ScaledAverage, EMA scaled to FPS
    Add ScaledAverage class, providing an Exponential Moving Average that
    adjusts the percentage according to the output device update rate.
    This makes it easier to apply a consistent level of smoothing in
    situations where one cannot interpolate to a well-defined ending
    point.
    
    For example...
    
    > var sampleDevice = new DeviceConfiguration (1, 1);
    > var sampleDeviceRO =
    >     new ReadOnlyDeviceConfiguration (sampleDevice);
    > var sampleFilter = new ScaledAverage (sampleDeviceRO);
    >
    > sampleFilter.TimeConstant = 300; // ms
    > sampleDevice.SetUpdateRate (50); // ms
    > Console.WriteLine (sampleFilter.WeightMultiplier);
    [prints "0.285714286"]
    > sampleDevice.SetUpdateRate (4);  // ms
    > Console.WriteLine (sampleFilter.WeightMultiplier);
    [prints "0.026315789"]
    
    ...the much lower percentage for the average in the second case would
    result in a longer time for the filter to reach the new value,
    accommodating the 12.5x faster update rate.
    
    Add unit tests.  So many tests.

---
+++ Actinic.Tests/Utilities/ScaledAverageTests.cs
@@ -0,0 +1,504 @@
+﻿//
+//  ScaledAverageTests.cs
+//
+//  Author:
+//       Shane Synan <digitalcircuit36939@gmail.com>
+//
+//  Copyright (c) 2019 FoxSoft
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using NUnit.Framework;
+using System;
+using System.Collections;
+
+using Actinic.Output;
+using Actinic.Utilities;
+
+namespace Actinic.Tests
+{
+	[TestFixture ()]
+	public class ScaledAverageTests
+	{
+		#region Construction
+
+		[Test]
+		public void Construct_Default ()
+		{
+			// [Arrange]
+			DeviceConfiguration sampleDevice =
+				new DeviceConfiguration (1, 1);
+			sampleDevice.SetUpdateRate (50);
+			ReadOnlyDeviceConfiguration sampleDeviceRO =
+				new ReadOnlyDeviceConfiguration (sampleDevice);
+
+			// [Act]
+			ScaledAverage sampleFilter = new ScaledAverage (sampleDeviceRO);
+
+			// [Assert]
+			Assert.That (sampleFilter.TimeConstant,
+				Is.EqualTo (0),
+				"Default time constant value does not match"
+			);
+		}
+
+		[Test]
+		public void Construct_Direct ()
+		{
+			// [Arrange]
+			DeviceConfiguration sampleDevice =
+				new DeviceConfiguration (1, 1);
+			sampleDevice.SetUpdateRate (50);
+			ReadOnlyDeviceConfiguration sampleDeviceRO =
+				new ReadOnlyDeviceConfiguration (sampleDevice);
+
+			const double customTimeConstant = 92.857142857;
+			// Weight multiplier is exhaustively checked in another test; this
+			// just verifies basic setup
+			const double expectedWeightMultiplier = 0.7;
+			const double tolerance = 0.001;
+
+			// [Act]
+			ScaledAverage sampleFilter =
+				new ScaledAverage (sampleDeviceRO, customTimeConstant);
+
+			// [Assert]
+			Assert.That (sampleFilter.TimeConstant,
+				Is.EqualTo (customTimeConstant),
+				"Custom time constant value does not match"
+			);
+
+			Assert.That (sampleFilter.WeightMultiplier,
+				Is.InRange (expectedWeightMultiplier - tolerance,
+					expectedWeightMultiplier + tolerance),
+				"Expected weight multiplier value does not match"
+			);
+		}
+
+		[Test]
+		public void Construct_Null_Throws ()
+		{
+			// [Arrange]
+			const ReadOnlyDeviceConfiguration invalidDeviceRO = null;
+
+			// [Act/Assert]
+			Assert.That (
+				delegate {
+					new ScaledAverage (invalidDeviceRO);
+				},
+				Throws.TypeOf<ArgumentNullException> ()
+				.With.Property ("ParamName").EqualTo ("DeviceConfig")
+			);
+		}
+
+		[Test]
+		public void Construct_NegativeConstant_Throws ()
+		{
+			// [Arrange]
+			DeviceConfiguration sampleDevice =
+				new DeviceConfiguration (1, 1);
+			sampleDevice.SetUpdateRate (50);
+			ReadOnlyDeviceConfiguration sampleDeviceRO =
+				new ReadOnlyDeviceConfiguration (sampleDevice);
+
+			// [Act/Assert]
+			Assert.That (
+				delegate {
+					new ScaledAverage (sampleDeviceRO, -1);
+				},
+				Throws.TypeOf<ArgumentOutOfRangeException> ()
+				.With.Property ("ParamName").EqualTo ("Constant")
+			);
+		}
+
+		#endregion
+
+		#region Properties - TimeConstant
+
+		/// <summary>
+		/// Test cases for time constants at 50 ms
+		/// </summary>
+		private class KnownTimeConstants
+		{
+			public static IEnumerable TestCases {
+				get {
+					yield return new TestCaseData (50.0, 1.0);
+					yield return new TestCaseData (51.0, 0.99009901);
+					yield return new TestCaseData (61.1, 0.900090009);
+					yield return new TestCaseData (92.857142857, 0.7);
+					yield return new TestCaseData (1000, 0.095238095);
+				}
+			}
+		}
+
+		[Test, TestCaseSource (typeof(KnownTimeConstants), "TestCases")]
+		public void TimeConstant_SetKnown_Test (
+			double customTimeConstant, double expectedWeightMultiplier)
+		{
+			// [Arrange]
+			const double customUpdateRate = 50;
+			DeviceConfiguration sampleDevice =
+				new DeviceConfiguration (1, 1);
+			sampleDevice.SetUpdateRate (customUpdateRate);
+			ReadOnlyDeviceConfiguration sampleDeviceRO =
+				new ReadOnlyDeviceConfiguration (sampleDevice);
+			ScaledAverage sampleFilter =
+				new ScaledAverage (sampleDeviceRO);
+
+			const double tolerance = 0.000001;
+
+			// Ensure the TimeConstant has been calculated at least once
+			sampleFilter.Filter (0, 0);
+
+			// [Act]
+			sampleFilter.TimeConstant = customTimeConstant;
+
+			// [Assert]
+			Assert.That (sampleFilter.TimeConstant,
+				Is.EqualTo (customTimeConstant),
+				"Time constant value does not match"
+			);
+
+			Assert.That (sampleFilter.WeightMultiplier,
+				Is.InRange (expectedWeightMultiplier - tolerance,
+					expectedWeightMultiplier + tolerance),
+				"Expected weight multiplier value does not match"
+			);
+		}
+
+		[Test]
+		public void TimeConstant_LessThanUpdateRate_Test ()
+		{
+			// [Arrange]
+			double customUpdateRate = 50;
+			double customTimeConstant = customUpdateRate - 1;
+			DeviceConfiguration sampleDevice =
+				new DeviceConfiguration (1, 1);
+			// Set update rate
+			sampleDevice.SetUpdateRate (customUpdateRate);
+			ReadOnlyDeviceConfiguration sampleDeviceRO =
+				new ReadOnlyDeviceConfiguration (sampleDevice);
+			ScaledAverage sampleFilter = new ScaledAverage (sampleDeviceRO);
+
+			// [Act]
+			sampleFilter.TimeConstant = customTimeConstant;
+
+			// [Assert]
+			Assert.That (sampleFilter.WeightMultiplier,
+				Is.EqualTo (1),
+				"Weight multiplier for time constants less than update rate " +
+				"does not match"
+			);
+		}
+
+		[Test]
+		public void TimeConstant_Negative_Throws ()
+		{
+			// [Arrange]
+			DeviceConfiguration sampleDevice =
+				new DeviceConfiguration (1, 1);
+			sampleDevice.SetUpdateRate (50);
+			ReadOnlyDeviceConfiguration sampleDeviceRO =
+				new ReadOnlyDeviceConfiguration (sampleDevice);
+			ScaledAverage sampleFilter =
+				new ScaledAverage (sampleDeviceRO);
+
+			// [Act/Assert]
+			Assert.That (
+				delegate {
+					sampleFilter.TimeConstant = -1;
+				},
+				Throws.TypeOf<ArgumentOutOfRangeException> ()
+				.With.Property ("ParamName").EqualTo ("TimeConstant")
+			);
+		}
+
+		#endregion
+
+		#region Properties - WeightMultiplier
+
+		// Other aspects of WeightMultiplier are tested in TimeConstant section
+
+		[Test]
+		public void WeightMultiplier_ZeroUpdateRate_Test ()
+		{
+			// [Arrange]
+			DeviceConfiguration sampleDevice =
+				new DeviceConfiguration (1, 1);
+			ReadOnlyDeviceConfiguration sampleDeviceRO =
+				new ReadOnlyDeviceConfiguration (sampleDevice);
+			ScaledAverage sampleFilter = new ScaledAverage (sampleDeviceRO);
+
+			// [Act]
+			// Ensure update rate is set to zero
+			sampleDevice.SetUpdateRate (0);
+
+			// [Assert]
+			Assert.That (sampleFilter.WeightMultiplier,
+				Is.EqualTo (1),
+				"Weight multiplier for zero update rate does not match"
+			);
+		}
+
+		#endregion
+
+		#region Functions - Filter (double)
+
+		[Test]
+		public void Filter_ApproachesQuickEnough_Test (
+			[Values (4, 51, 1000)] double customTimeConstant,
+			[Values (4, 50)] double customUpdateRate,
+			[Values (-100, -1, 0, 1)] double startingValue,
+			[Values (-1, 0, 1, 100)] double endingValue)
+		{
+			// [Arrange]
+			DeviceConfiguration sampleDevice =
+				new DeviceConfiguration (1, 1);
+			sampleDevice.SetUpdateRate (customUpdateRate);
+			ReadOnlyDeviceConfiguration sampleDeviceRO =
+				new ReadOnlyDeviceConfiguration (sampleDevice);
+			ScaledAverage sampleFilter =
+				new ScaledAverage (sampleDeviceRO, customTimeConstant);
+
+			// NOTE: To fully test value approaching, at least some starting
+			// and ending values must not be the same, and at least one time
+			// constant should be 2.5x larger than the update rate.
+
+			// Expected iterations for 63%: ratio of customTimeConstant and
+			// customUpdateRate
+			int iterationsPartial =
+				(int)(customTimeConstant / customUpdateRate) + 1;
+
+			// Expected iterations for goal: a "long time"
+			int iterationsLongterm = (int)(1000 / sampleDevice.FactorTime);
+
+			// Reaching 63% of the way there
+			const double percentage = 0.63;
+			double delta = endingValue - startingValue;
+			double thresholdPartial =
+				startingValue + (delta * percentage);
+
+			double tolerance = delta * 0.1;
+			// Ensure tolerance exists for same values
+			double variance = (tolerance != 0 ? tolerance : 0.01);
+
+			// Find the from/to values for 63%
+			double thresholdPartialFrom = 0;
+			double thresholdPartialTo = 0;
+			if (startingValue == endingValue) {
+				// Keeping the same value
+				thresholdPartialFrom = endingValue - variance;
+				thresholdPartialTo = endingValue + variance;
+			} else if (startingValue < endingValue) {
+				// Target: increasing past 63%
+				thresholdPartialFrom = thresholdPartial;
+				thresholdPartialTo = endingValue;
+			} else {
+				// Target: decreasing past 63%
+				thresholdPartialFrom = endingValue;
+				thresholdPartialTo = thresholdPartial;
+			}
+
+			// Reaching most of the way there
+			double thresholdLongtermFrom = 0;
+			double thresholdLongtermTo = 0;
+			if (variance >= 0) {
+				// Trending positive
+				thresholdLongtermFrom = endingValue - variance;
+				thresholdLongtermTo = endingValue + variance;
+			} else {
+				// Trending negative
+				thresholdLongtermFrom = endingValue + variance;
+				thresholdLongtermTo = endingValue - variance;
+			}
+
+			// [Act]
+			// Find the 63% value
+			double currentValuePartial = startingValue;
+			for (int i = 0; i < iterationsPartial; i++) {
+				currentValuePartial =
+					sampleFilter.Filter (currentValuePartial, endingValue);
+			}
+
+			// Find the longterm value
+			double currentValueLongterm = currentValuePartial;
+			for (int i = 0; i < iterationsPartial; i++) {
+				currentValueLongterm =
+					sampleFilter.Filter (currentValueLongterm, endingValue);
+			}
+
+			// [Assert]
+			Assert.That (currentValuePartial,
+				Is.InRange (thresholdPartialFrom, thresholdPartialTo),
+				String.Format (
+					"Value does not reach 63% ({0}) after {1} iterations",
+					thresholdPartial, iterationsPartial
+				)
+			);
+
+			Assert.That (currentValueLongterm,
+				Is.InRange (thresholdLongtermFrom,
+					thresholdLongtermTo),
+				String.Format (
+					"Value does not match end ({0}) after {1} iterations",
+					endingValue, iterationsLongterm
+				)
+			);
+		}
+
+		[Test]
+		public void Filter_ApproachesSlowEnough_Test (
+			[Values (4, 50)] double customUpdateRate,
+			[Values (-100, -1, 0, 1)] double startingValue,
+			[Values (-1, 0, 1, 100)] double endingValue)
+		{
+			// [Arrange]
+			// Pick a time constant that ensures values won't change too quickly
+			double customTimeConstant = customUpdateRate * 2.5;
+
+			DeviceConfiguration sampleDevice =
+				new DeviceConfiguration (1, 1);
+			sampleDevice.SetUpdateRate (customUpdateRate);
+			ReadOnlyDeviceConfiguration sampleDeviceRO =
+				new ReadOnlyDeviceConfiguration (sampleDevice);
+			ScaledAverage sampleFilter =
+				new ScaledAverage (sampleDeviceRO, customTimeConstant);
+
+			// Reaching 63% of the way there
+			const double percentage = 0.63;
+			double delta = endingValue - startingValue;
+			double thresholdPartial =
+				startingValue + (delta * percentage);
+
+			double tolerance = delta * 0.1;
+			// Ensure tolerance exists for same values
+			double variance = (tolerance != 0 ? tolerance : 0.01);
+
+			// Find the from/to values for 63%
+			double thresholdFirstFrom = 0;
+			double thresholdFirstTo = 0;
+			if (startingValue == endingValue) {
+				// Keeping the same value
+				thresholdFirstFrom = endingValue - variance;
+				thresholdFirstTo = endingValue + variance;
+			} else if (startingValue < endingValue) {
+				// First: increasing but not yet past 63%
+				thresholdFirstFrom = startingValue;
+				thresholdFirstTo = thresholdPartial;
+			} else {
+				// First: decreasing but not yet past 63%
+				thresholdFirstFrom = thresholdPartial;
+				thresholdFirstTo = startingValue;
+			}
+
+			// [Act]
+			// Find the first value
+			double firstValue =
+				sampleFilter.Filter (startingValue, endingValue);
+
+			// [Assert]
+			Assert.That (firstValue,
+				Is.InRange (thresholdFirstFrom, thresholdFirstTo),
+				String.Format (
+					"Value goes past 63% ({0}) too soon, after 1 iteration",
+					thresholdPartial
+				)
+			);
+		}
+
+		#endregion
+
+		#region Functions - Filter (non-double)
+
+		[Test]
+		public void Filter_TypesMatch_Test (
+			[Values (0, 1, 255)] byte startingValue,
+			[Values (0, 1, 255)] byte endingValue)
+		{
+			// [Arrange]
+			const double customUpdateRate = 50;
+			const double customTimeConstant = 61;
+			DeviceConfiguration sampleDevice =
+				new DeviceConfiguration (1, 1);
+			sampleDevice.SetUpdateRate (customUpdateRate);
+			ReadOnlyDeviceConfiguration sampleDeviceRO =
+				new ReadOnlyDeviceConfiguration (sampleDevice);
+			ScaledAverage sampleFilter =
+				new ScaledAverage (sampleDeviceRO, customTimeConstant);
+
+			const int iterations = 100;
+
+			// [Act]
+			// Ensure that functions match regardless of type
+			double currentValueDouble = startingValue;
+			int currentValueInt = startingValue;
+			byte currentValueByte = startingValue;
+
+			for (int i = 0; i < iterations; i++) {
+				currentValueDouble =
+					(int)sampleFilter.Filter (currentValueDouble, endingValue);
+				currentValueInt =
+					(int)sampleFilter.Filter (currentValueInt, endingValue);
+				currentValueByte =
+					(byte)sampleFilter.Filter (currentValueByte, endingValue);
+			}
+
+			// [Assert]
+			Assert.That (currentValueInt,
+				Is.EqualTo (currentValueDouble),
+				"Integer value does not match double value"
+			);
+
+			Assert.That (currentValueByte,
+				Is.EqualTo (currentValueDouble),
+				"Byte value does not match double value"
+			);
+		}
+
+		#endregion
+
+
+		#region Functions - ToString
+
+		[Test]
+		public void ToString_Test ()
+		{
+			// [Arrange]
+			DeviceConfiguration sampleDevice =
+				new DeviceConfiguration (1, 1);
+			sampleDevice.SetUpdateRate (50);
+			ReadOnlyDeviceConfiguration sampleDeviceRO =
+				new ReadOnlyDeviceConfiguration (sampleDevice);
+			const double customTimeConstant = 92.857142857;
+			ScaledAverage sampleFilter =
+				new ScaledAverage (sampleDeviceRO, customTimeConstant);
+
+			// Prepare the expected result of converting to string
+			string expectedResult =
+				string.Format (
+					"[ScaledAverage: TimeConstant={0}, WeightMultiplier={1}]",
+					sampleFilter.TimeConstant,
+					sampleFilter.WeightMultiplier
+				);
+
+			// [Act/Assert]
+			Assert.That (sampleFilter.ToString (),
+				Is.EqualTo (expectedResult)
+			);
+		}
+
+		#endregion
+
+	}
+}
+
---
+++ Actinic/Utilities/ScaledAverage.cs
@@ -0,0 +1,259 @@
+﻿//
+//  ScaledAverage.cs
+//
+//  Author:
+//       Shane Synan <digitalcircuit36939@gmail.com>
+//
+//  Copyright (c) 2019 FoxSoft
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+
+using Actinic.Output;
+
+namespace Actinic.Utilities
+{
+	/// <summary>
+	/// Exponential moving average filter that scales according to update rate.
+	/// </summary>
+	public class ScaledAverage
+	{
+		#region Constructors
+
+		/// <summary>
+		/// Initializes a new exponential moving average filter.
+		/// </summary>
+		/// <param name="DeviceConfig">Active output device configuration.</param>
+		/// <param name="Constant">The time constant of the scaled average in milliseconds.</param>
+		public ScaledAverage (
+			ReadOnlyDeviceConfiguration DeviceConfig, double Constant = 0)
+		{
+			if (DeviceConfig == null) {
+				// Need a valid device configuration
+				throw new ArgumentNullException ("DeviceConfig");
+			}
+			if (Constant < 0) {
+				throw new ArgumentOutOfRangeException (
+					"Constant",
+					"Constant must be greater than zero."
+				);
+			}
+			deviceConfig = DeviceConfig;
+			TimeConstant = Constant;
+		}
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// Gets or sets the time constant of the scaled average in
+		/// milliseconds, e.g. the time it takes to reach 63.2% of the new
+		/// value.
+		/// <remarks>
+		/// This does not account for applying the first value.  When first
+		/// applying the filter, the time constant instead roughly indicates how
+		/// long it will take to reach 90% of the new value.
+		/// </remarks>
+		/// </summary>
+		/// <value>The time constant of the scaled average in milliseconds.</value>
+		public double TimeConstant {
+			get {
+				return timeConstant;
+			}
+			set {
+				if (value < 0) {
+					throw new ArgumentOutOfRangeException (
+						"TimeConstant",
+						"TimeConstant must be greater than zero."
+					);
+				}
+				if (timeConstant != value) {
+					timeConstant = value;
+					// Clear the stored weighting multiplier
+					cachedWeightMultiplier = -1;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Gets the multiplier for maintaining a fixed amount of smoothing
+		/// regardless of the current animation speed.
+		/// <remarks>
+		/// In most cases, you should use <see cref="Filter()"/> instead.
+		/// </remarks>
+		/// </summary>
+		/// <value>The weighting multiplier as a positive decimal.</value>
+		public double WeightMultiplier {
+			get {
+				if (cachedWeightMultiplier == -1
+				    || lastFactorTime != deviceConfig.FactorTime) {
+					// Cleared or never set, or time factor has changed, need to
+					// calculate
+					CalculateWeightMultiplier ();
+				}
+				return cachedWeightMultiplier;
+			}
+		}
+
+		#endregion
+
+		#region Shared Functions
+
+		/// <summary>
+		/// Applies the new value onto the past values, filtering using the
+		/// chosen <see cref="TimeConstant"/>.
+		/// </summary>
+		/// <returns>The filtered value, combining the past values and new value.</returns>
+		/// <param name="Past">Past values.</param>
+		/// <param name="New">New value.</param>
+		public double Filter (double Past, double New)
+		{
+			// Apply the exponential moving average filter
+			//
+			// WeightMultiplier represents the "freshness" of the value, i.e.
+			// how much to emphasize the new value over the old value.
+			return (
+			    ((1 - WeightMultiplier) * Past) + (WeightMultiplier * New)
+			);
+		}
+
+		/// <summary>
+		/// Applies the new value onto the past values, filtering using the
+		/// chosen <see cref="TimeConstant"/>.
+		/// </summary>
+		/// <returns>The filtered value, combining the past values and new value.</returns>
+		/// <param name="Past">Past values.</param>
+		/// <param name="New">New value.</param>
+		public int Filter (int Past, int New)
+		{
+			// Call the most general version, double
+			return (int)Filter ((double)Past, New);
+		}
+
+		/// <summary>
+		/// Applies the new value onto the past values, filtering using the
+		/// chosen <see cref="TimeConstant"/>.
+		/// </summary>
+		/// <returns>The filtered value, combining the past values and new value.</returns>
+		/// <param name="Past">Past values.</param>
+		/// <param name="New">New value.</param>
+		public byte Filter (byte Past, byte New)
+		{
+			// Call the most general version, double
+			return (byte)Filter ((double)Past, New);
+		}
+
+		/// <summary>
+		/// Returns a string that represents the current
+		/// <see cref="ScaledAverage"/>.
+		/// </summary>
+		/// <returns>A string that represents the current <see cref="ScaledAverage"/>.</returns>
+		public override string ToString ()
+		{
+			return string.Format (
+				"[ScaledAverage: TimeConstant={0}, WeightMultiplier={1}]",
+				TimeConstant, WeightMultiplier);
+		}
+
+		#endregion
+
+		#region Internal
+
+		/// <summary>
+		/// Calculates the internal weight multiplier.
+		/// </summary>
+		private void CalculateWeightMultiplier ()
+		{
+			// Track the last-used FactorTime
+			lastFactorTime = deviceConfig.FactorTime;
+
+			if (lastFactorTime <= 0) {
+				// Dividing by zero is a no-go, assume the percentage of 1 to
+				// ensure filtering always has an effect
+				cachedWeightMultiplier = 1;
+			} else if (TimeConstant < lastFactorTime) {
+				// If requested delay amount is less than FactorTime, the
+				// percentage will always be greater than 1.  Cap to 1.
+				cachedWeightMultiplier = 1;
+			} else {
+				// Find the weighting multiplier
+				// This is based on references for exponential moving averages.
+				// See http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:moving_averages
+
+				cachedWeightMultiplier =
+					1 / (((TimeConstant / lastFactorTime) + 1) / 2);
+
+				// Derivation:
+				// ((2÷percent)−1)×FactorTime = ms
+				// (ms ÷ FactorTime) = ((2÷percent)-1)
+				// (ms ÷ FactorTime) + 1 = (2÷percent)
+				// ((ms ÷ FactorTime) + 1) ÷ 2 = (1÷percent)
+				// percent = 1 ÷ (((ms ÷ FactorTime) + 1) ÷ 2)
+				//
+				// (If ms < FactorTime, set percent = 1)
+				//
+				// Finding the time period:
+				// x=0
+				// x=((x×(1−p))+(255×p))
+				// p = 0.7 freshness (e.g. 0.3 of past value)
+				// ((2÷0.7)−1) = 1.85 periods, which at 50 ms per period...
+				// ((2÷0.7)−1)×50 = 92.8 ms to 63% mark
+				//
+				// With p = 0.9
+				// ((2÷0.9)−1) = 1.22 periods
+				// ((2÷0.9)−1)×50 = 61.1 ms to 63% mark
+				//
+				// Examples:
+				// Using this equation: 1÷(((61ms ÷ FactorTime)+1)÷2)
+				// For 50ms, 1÷(((61 ÷ 50)+1)÷2) = 0.900900901 (rounding errors)
+				// For 4ms, 1÷(((61 ÷ 4)+1)÷2) = 0.123076923
+				// To check...
+				// ((2÷0.123076923)−1)×4 = 61.0 ms
+				//
+				// Converting old values to TimeConstant:
+				// ms = ((2÷percent)−1)×50
+				// (Assuming 50 ms update rate of the first LED strand)
+				//
+				// NOTE: SmoothingAmount from AbstractAnimation specifies the
+				// "oldness" value, NOT the "freshness" value.  To convert it,
+				// go with...
+				// ms = ((2÷(1−smoothing_percent)−1)×50
+			}
+		}
+
+		/// <summary>
+		/// The weighting multiplier as a positive decimal.
+		/// </summary>
+		private double cachedWeightMultiplier = 0;
+
+		/// <summary>
+		/// The last-used time factor as a positive decimal, or -1 if not used.
+		/// </summary>
+		private double lastFactorTime = -1;
+
+		/// <summary>
+		/// The time constant of the scaled average in milliseconds.
+		/// </summary>
+		private double timeConstant = -1;
+
+		/// <summary>
+		/// The device configuration used for scaling the smoothing amount.
+		/// </summary>
+		private ReadOnlyDeviceConfiguration deviceConfig;
+
+		#endregion
+	}
+}
+
